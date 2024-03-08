9dad3f544bbf64b1e3790ba180d7306b11ec50d2
Stuti Sinha <stutisinha712@gmail.com>
2024-03-08 23:56:21 +0530

    list maker created

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,50 @@ import { useState } from "react"
 import "./styles.css"
 export default function App() {
   const [newItem, setNewItem]=useState(" ")
+  const [todos, setTodos]=useState([])
+  function handleSubmit(e){
+    e.preventDefault()
+    setTodos(currentTodos=>{
+      return [
+        ...currentTodos,
+        {
+          id:crypto.randomUUID(),
+          text: newItem,
+          completed: false
+        },
+      ]
+    })
+  }
   return(
     <>
-   <form className="new-item-form">
+   <form onSubmit={handleSubmit} className="new-item-form">
     <div className="form-row">
       <label htmlFor="item"> New Item </label>
         
-        <input id="item" 
-        type="text" 
+        <input 
         value={newItem} 
         onChange={(e)=>setNewItem(e.target.value)}
+        id="item" 
+        type="text" 
+        
         />
+        </div>
         <button className="btn">Add</button>
      
-    </div>
+    
     </form>
     <h1 className="header">To-do List</h1>
     <ul className="list">
-      <li>
+     { todos.map (todo=> {
+      return <li key={todo.id}>
         <label>
-        <input type="checkbox" />
-        Item 1
+        <input type="checkbox" checked={todo.completed} />
+        {todo.text}
         </label>
         <button className="btn btn-danger">Delete</button>
+    
       </li>
+       })}
     </ul>
     </>
   )
