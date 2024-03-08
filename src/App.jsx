cc4ed8465df49febaf2c08933262da9123052a48
Stuti Sinha <stutisinha712@gmail.com>
2024-03-09 00:03:16 +0530

    checking and deleting

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,22 @@ export default function App() {
         },
       ]
     })
+    setNewItem(" ")
+  }
+  function toggleTodo(id,completed){
+    setTodos(currentTodos=>{
+      return currentTodos.map(todo=>{
+        if(todo.id===id){
+          return {...todo,completed}
+        }
+        return todo
+      })
+    })
+  }
+  function deleteTodo(){
+    setTodos(currentTodos=>{
+      return currentTodos.filter(todo=>!todo.completed)
+    })
   }
   return(
     <>
@@ -39,10 +55,11 @@ export default function App() {
      { todos.map (todo=> {
       return <li key={todo.id}>
         <label>
-        <input type="checkbox" checked={todo.completed} />
+        <input type="checkbox" checked={todo.completed}
+        onChange={e=>toggleTodo (todo.id,e.target.checked)} />
         {todo.text}
         </label>
-        <button className="btn btn-danger">Delete</button>
+        <button onClick={()=> deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
     
       </li>
        })}
